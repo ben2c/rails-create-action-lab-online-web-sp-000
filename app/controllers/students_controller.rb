4cfa4a6681341fabcc60b93b47fdb5e2ea752706
Benjamin Cheng <bencheng200@gmail.com>
2021-03-16 15:20:23 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -16,8 +16,8 @@ class StudentsController < ApplicationController
     @student.first_name = params[:first_name]
     @student.last_name = params[:last_name]
     @student.save
-    redirect_to student_path
-    
+    redirect_to student_path(@student)
+
   end
 
 end
